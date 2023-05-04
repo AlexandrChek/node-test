b080a278c62ea181baec2838bf01057061597656
Chekcheev_Alexandr <chekcheev.alex@gmail.com>
2023-05-04 12:44:02 +0300

    add start

--- public/fishes/fishes_controller.js
+++ public/fishes/fishes_controller.js
@@ -1,9 +1,26 @@
 const Fishes = require('./fishes_router.js')
 
+import parallel from 'async/parallel'
+
 exports.home = function(req, res) {
     res.send('This is fisfes homepage')
 }
 
 exports.about = function(rec, res) {
-    res.send('It`s about fishes')
+    parallel(
+        [
+            function one() {return 'one fish'},
+            function two() {return 'two fishes'},
+            function three() {return 'three fishes'}
+        ],
+        function(err, results) {
+            let template = `
+            <p>It's about fishes.</p>
+            <p>${results.one()}</p>
+            <p>${results.two()}</p>
+            <p>${results.three()}</p>
+            `
+            res.send(template)
+        }
+    )
 }
